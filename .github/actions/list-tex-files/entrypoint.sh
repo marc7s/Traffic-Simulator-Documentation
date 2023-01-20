a6c307a54ff60cd586b0f9ae8dcba958969dda4e
marc7s <marcus.schagerberg@gmail.com>
2023-01-20 22:01:03 +0100

    Replaced newlines in workflow

--- .github/actions/list-tex-files/entrypoint.sh
+++ .github/actions/list-tex-files/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 
 echo "Searching folder '$1'"
-files=$(find $1 -type f -name "*.tex")
+files=$(find $1 -type f -name "*.tex" | tr '\n' ' ')
 echo "$files"
 echo "files=toJSON($files)" >> $GITHUB_OUTPUT

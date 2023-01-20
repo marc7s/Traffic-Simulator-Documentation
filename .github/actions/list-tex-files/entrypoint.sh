e9bf7de8b5843a51767d0c7fba137eb23da81b35
marc7s <marcus.schagerberg@gmail.com>
2023-01-20 22:39:39 +0100

    New regex

--- .github/actions/list-tex-files/entrypoint.sh
+++ .github/actions/list-tex-files/entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/sh -l
 
 echo "Searching folder '$1'"
-#jstring=jstring | sed 's/\(.*\), /\1/'
 files=$(find $1 -type f -name "*.tex" | tr '\n' ', ')
-files=files | sed -r 's/(.*), /\1/'
-echo "$files"
+files=files | sed -r 's/(.*),/\1/'
+echo "[$files]"
 echo "files=$files" >> $GITHUB_OUTPUT

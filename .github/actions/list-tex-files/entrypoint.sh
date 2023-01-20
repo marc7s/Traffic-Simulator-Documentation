66299ccc867a8c1958243a8074dae88dedae70a1
marc7s <marcus.schagerberg@gmail.com>
2023-01-20 22:27:20 +0100

    Added jstring

--- .github/actions/list-tex-files/entrypoint.sh
+++ .github/actions/list-tex-files/entrypoint.sh
@@ -1,6 +1,16 @@
 #!/bin/sh -l
 
 echo "Searching folder '$1'"
-files=$(find $1 -type f -name "*.tex" | tr '\n' ' ')
+files=()
+while IFS = read -r -d $'\0'; do
+    files += ("$REPLY")
+done < <(find $1 -type f -name "*.tex" -print0)
+jstring="["
+for file in "${array[@]}"; do 
+    jstring+="'$file', ";
+done
+jstring=jstring | sed 's/\(.*\), /\1/'
+#files=$(find $1 -type f -name "*.tex" | tr '\n' ', ')
 echo "$files"
-echo "files=toJSON($files)" >> $GITHUB_OUTPUT
+echo "$jstring"
+echo "files=$jstring" >> $GITHUB_OUTPUT

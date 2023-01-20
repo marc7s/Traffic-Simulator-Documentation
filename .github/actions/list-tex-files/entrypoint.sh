e872679c1fa2030ba189af7ba98e2a8fbf40ccdd
marc7s <marcus.schagerberg@gmail.com>
2023-01-20 21:57:23 +0100

    Convert file list to array

--- .github/actions/list-tex-files/entrypoint.sh
+++ .github/actions/list-tex-files/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -l
 
 echo "Searching folder '$1'"
-files=$(find $1 -type f -name "*.tex")
+#files=$(find $1 -type f -name "*.tex")
+readarray -d '' files < <(find $1 -type f -name "*.tex")
+echo "$files"
 echo "files=$files" >> $GITHUB_OUTPUT

f7e6b226c67e6b38c77920291d86d1188f98a2b8
marc7s <marcus.schagerberg@gmail.com>
2023-01-20 22:31:42 +0100

    Reverted to shell

--- .github/actions/list-tex-files/entrypoint.sh
+++ .github/actions/list-tex-files/entrypoint.sh
@@ -1,16 +1,8 @@
-#!/bin/bash -l
+#!/bin/sh -l
 
 echo "Searching folder '$1'"
-files=()
-while IFS = read -r -d $'\0'; do
-    files += ("$REPLY")
-done < <(find $1 -type f -name "*.tex" -print0)
-jstring="["
-for file in "${array[@]}"; do 
-    jstring+="'$file', ";
-done
-jstring=jstring | sed 's/\(.*\), /\1/'
+#jstring=jstring | sed 's/\(.*\), /\1/'
 #files=$(find $1 -type f -name "*.tex" | tr '\n' ', ')
+files=files | sed 's/\(.*\), /\1/'
 echo "$files"
-echo "$jstring"
-echo "files=$jstring" >> $GITHUB_OUTPUT
+echo "files=$files" >> $GITHUB_OUTPUT

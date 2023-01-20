f72113f56bb9cf67fc37651ecbfe2cf9732fb5b3
marc7s <marcus.schagerberg@gmail.com>
2023-01-20 23:01:28 +0100

    Removed final replacement

--- .github/actions/list-tex-files/entrypoint.sh
+++ .github/actions/list-tex-files/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 
 echo "Searching folder '$1'"
-files=$(find $1 -type f -name "*.tex" | tr "\n" "','" | sed 's/\(.*\),/\1]/')
+files=$(find $1 -type f -name "*.tex" | tr "\n" "','"# | sed 's/\(.*\),/\1]/')
 #files=files | sed -r 's/(.*),/\1/'
 echo "['$files]"
 echo "files=$files" >> $GITHUB_OUTPUT

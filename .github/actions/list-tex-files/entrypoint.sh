df8a8ec49ecab4f689d8d0c69e2f19c500ae204d
marc7s <marcus.schagerberg@gmail.com>
2023-01-20 22:44:40 +0100

    Removed comma replacement

--- .github/actions/list-tex-files/entrypoint.sh
+++ .github/actions/list-tex-files/entrypoint.sh
@@ -2,6 +2,6 @@
 
 echo "Searching folder '$1'"
 files=$(find $1 -type f -name "*.tex" | tr "\n" "','")
-files=files | sed -r 's/(.*),/\1/'
+#files=files | sed -r 's/(.*),/\1/'
 echo "['$files]"
 echo "files=$files" >> $GITHUB_OUTPUT

4e4bf0a0b7cdb6818ae9dd586fb88684cd2484d3
marc7s <marcus.schagerberg@gmail.com>
2023-01-21 03:25:15 +0100

    Finished CI/CD for compiling LaTeX documents

--- .github/actions/list-tex-files/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -l
-
-echo "Searching folder '$1'"
-files=$(find $1 -type f -name "*.tex" | tr "\n" ">" | sed 's/\(.*\)>/\1</' | sed 's/>/", "/g' | sed 's/</"/g')
-# | sed "s/\(.*\)\$/\1\']/" | sed "s/\$/\',\'/g")
-#files=files | sed -r 's/(.*),/\1/'
-fileArray="[\"${files}]"
-echo "${fileArray}"
-echo "files=${fileArray}" >> $GITHUB_OUTPUT

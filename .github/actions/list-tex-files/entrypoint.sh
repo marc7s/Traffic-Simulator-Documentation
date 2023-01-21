db12ae2c6d9d3b94f98154a5bbec21d905fbe81f
marc7s <marcus.schagerberg@gmail.com>
2023-01-21 01:31:59 +0100

    More chains

--- .github/actions/list-tex-files/entrypoint.sh
+++ .github/actions/list-tex-files/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 
 echo "Searching folder '$1'"
-files=$(find $1 -type f -name "*.tex" | tr "\n" ">" | sed 's/\(.*\)>/\1</')
+files=$(find $1 -type f -name "*.tex" | tr "\n" ">" | sed 's/\(.*\)>/\1</' | sed 's/>/",/g' | sed 's/</"/g')
 # | sed "s/\(.*\)\$/\1\']/" | sed "s/\$/\',\'/g")
 #files=files | sed -r 's/(.*),/\1/'
 echo "$files"
